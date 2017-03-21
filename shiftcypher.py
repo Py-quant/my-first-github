307122e8ec6bbff8521442d5c637b024fdab5268
farcekarter <farcekarter@users.noreply.github.com>
2017-03-20 23:23:22 -0700

    fixed

--- shiftcypher.py
+++ shiftcypher.py
@@ -1,29 +1,25 @@
+
+# This function will return a number between 0 and 25
+# that corresponds to the position in the alphabet of
+# the character.
 def getNumForChar(char):
 	return ord(char)-65
 
+# This function will return a character that appears at
+# position num in the alphabet.
 def getCharForNum(num):
 	return chr(num+65)
 
-def encryptString(mystr,shift):
-	newStr = string_b
-	return newStr
 
-def decryptString(mystr,shift):
-	newStr = string_a
-	return newStr
+def encryptString(string_b,shift):
+	shift = string_b
+	return shift
 
-string_a = "ABCXYZ"
-string_b = "CDEZAB"
-test_str_a = encryptString(string_a,2)
-# This should print CDEZAB
-print test_str_a
-# If the encryption is correct, this will print True
-print test_str_a == string_b
-# Now let's decrypt
-test_str_b = decryptString(test_str_a,2)
-# This should print ABCXYZ
-print test_str_b
-# If the decryption is correct, this will print True
-print test_str_b == string_a
+def decryptString(string_a,shift):
+	shift = string_a
+	return shift
+
+
+
 
 
